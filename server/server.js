84d683c907f043b43318b09e66d46a7d4ecaccb4
efleming111 <efleming111@yahoo.com>
2019-02-07 20:36:05 -0700

    Finished MVP

--- server/server.js
+++ server/server.js
@@ -3,25 +3,50 @@ const express = require('express');
 const server = express();
 server.use(express.json());
 
-const users = [
-    {name: "jim"},
-    {name: "jane"}
+let users = [
+    {id: 1, name: "jim"},
+    {id: 2, name: "jane"}
 ]
+
+function resetUsers(){
+    users = [
+        {id: 1, name: "jim"},
+        {id: 2, name: "jane"}
+    ]
+}
 
 server.get('/users', async (req, res)=>{
     res.status(200).json({users: users});
 })
 
-server.get('/users/:id', async (req, res)=>{
-    
-})
-
 server.post('/users', async (req, res)=>{
-    
+    const body = req.body;
+    if(body.id && body.name){
+        users.push(body);
+        res.status(201).send({id: 3});
+    }
+    else{
+        res.status(400).json({
+            errorMessage: 'Failed to add user to database'
+        })
+    }
 })
 
-server.delete('/users/id', async (req, res)=>{
-    
+server.delete('/users/:id', async (req, res)=>{
+    const id = parseInt(req.params.id);
+    const userToDelete = users.find(user=>user.id === id)
+    if(userToDelete){
+        users.pop();
+        res.status(200).json({id: userToDelete.id});
+    }
+    else{
+        res.status(400).json({
+            errorMessage: 'Failed to remove user from database'
+        })
+    }
 })
 
-module.exports =  server;
+module.exports =  {
+    server,
+    resetUsers
+}
--- server/server.spec.js
+++ server/server.spec.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 
-const server = require('./server');
+const {server, resetUsers} = require('./server');
 
 describe('the route handlers', ()=>{
     describe('get /users', ()=>{
@@ -17,9 +17,67 @@ describe('the route handlers', ()=>{
         it('returns an array of users', async ()=>{
             const res = await request(server).get('/users');
             expect(res.body.users).toEqual([
-                {name: "jim"},
-                {name: "jane"}
+                {id: 1, name: "jim"},
+                {id: 2, name: "jane"}
             ])
+        })
+    })
+
+    describe('post /users', ()=>{
+        afterEach(async()=>{
+            await resetUsers();
+        })
+        it('returns status code 201', async ()=>{
+            const body = {id: 3, name: "tom"}
+            const res = await request(server).post('/users').send(body);
+            expect(res.status).toBe(201);
+        })
+
+        it('returns an id', async ()=>{
+            const body = {id: 3, name: "tom"}
+            const res = await request(server).post('/users').send(body);
+            expect(res.body).toEqual({id: 3});
+        })
+
+        it('returns status code 400 on failure', async ()=>{
+            const body = {name: "tom"}
+            const res = await request(server).post('/users').send(body);
+            expect(res.status).toBe(400);
+        })
+
+        it('returns an error message on failure', async ()=>{
+            const body = {name: "tom"}
+            const res = await request(server).post('/users').send(body);
+            expect(res.body).toEqual({
+                errorMessage: 'Failed to add user to database'
+            })
+        })
+    })
+    
+    describe('delete /users/:id', ()=>{
+        afterEach(async()=>{
+            await resetUsers();
+        })
+        it('returns status code 200', async ()=>{
+            const res = await request(server).delete('/users/2');
+            expect(res.status).toBe(200);
+        })
+
+        it('returns the deleted users id', async ()=>{
+            const res = await request(server).delete('/users/2');
+            expect(res.body).toEqual({id: 2});
+        })
+        
+        it('returns status code 400 on failure', async ()=>{
+            const res = await request(server).delete('/users/3');
+            expect(res.status).toBe(400);
+        })
+
+        it('returns an error message on failure', async ()=>{
+            const res = await request(server).delete('/users/3');
+            expect(res.body).toEqual({
+                errorMessage: 'Failed to remove user from database'
+            })
         })
     })
 })
